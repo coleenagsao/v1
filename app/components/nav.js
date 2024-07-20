ded92a7bdb24fd901666588954db692f062b77b6
Coleen Agsao <caagsao@up.edu.ph>
2024-07-21 04:14:54 +0800

    feat: implement onClick event to mobile menu items to close when a link is clicked

--- app/components/nav.js
+++ app/components/nav.js
@@ -89,6 +89,7 @@ export default function Nav(){
                         <a
                           key={item.name}
                           href={item.url}
+                          onClick={() => setMobileMenuOpen(false)}
                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-200 "
                         >
                           <span className="hover:text-blue-300">{item.name}</span>
