ee27345d34c1df78fb2072b210866872affa0c23
Coleen Agsao <caagsao@up.edu.ph>
2024-07-20 23:04:21 +0800

    feat: add hero responsiveness on name and description

--- app/components/sections/hero.js
+++ app/components/sections/hero.js
@@ -5,46 +5,39 @@ import Image from "next/image";
 
 export default function Hero(){
     return (
-        <div className="relative isolate px-6 pt-14 lg:px-8">
-            <div
-              aria-hidden="true"
-              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            >
+        <div className="relative isolate px-6 pt-14 lg:px-8 h-screen">
+            {/* Gradient Background */}
+            <div aria-hidden="true" className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
               <div
                 style={{
                   clipPath:
                     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                 }}
-                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80c8ff] to-[#89a8fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              />
-            </div>
-            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-200/10 hover:ring-gray-100/20">
-                  Announcing our next round of funding.{' '}
-                  <a href="#" className="font-semibold text-indigo-600">
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Read more <span aria-hidden="true">&rarr;</span>
-                  </a>
-                </div>
+                  className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80c8ff] to-[#89a8fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                />
               </div>
-              <div className="text-center">
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Data to enrich your online business
+
+            <div className="mx-auto max-w-4xl lg:max-w-5xl py-32 sm:py-48 lg:py-56 ">
+              {/* Name and Description */}
+              <div>
+                {/* Greeting */}
+                <h1 className="font-mono tracking-tight text-gray-200 text-xl lg:text-xl mb-6">
+                  Halo!<span aria-hidden="true">👋</span>
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                  fugiat veniam occaecat fugiat aliqua.
+                {/* Name */}
+                <h1 className="text-4xl font-bold tracking-tight text-gray-200 md:text-5xl lg:text-7xl">
+                  I<span aria-hidden="true">&apos;</span>m <span className="text-blue-300">Coleen Agsao</span>.
+                </h1>
+                {/* Description */}
+                <p className="mt-6 text-lg leading-8 text-gray-200 max-w-2xl md:max-w-3xl lg:max-w-5xl">
+                  A software engineer focused on building above-average digital experiences. Currently, I’m focused on building a hospital information system on the side with <a href="https://jrrmmc.gov.ph/" className="text-blue-300">JRRMC</a>.
                 </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <a
-                    href="#"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Get started
-                  </a>
-                  <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                    Learn more <span aria-hidden="true">→</span>
+              </div>
+              <div className="sm:mb-8 sm:flex sm:justify-center py-6">
+                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-200/10 hover:ring-gray-100/20">
+                  <a href="#" className="font-semibold text-blue-500">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    coleentherese.agsao@gmail.com
                   </a>
                 </div>
               </div>
