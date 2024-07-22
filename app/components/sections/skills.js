011ae7ff1e368060816cdca95b9e3f8c6c22ed39
Coleen Agsao <caagsao@up.edu.ph>
2024-07-22 20:06:07 +0800

    style: update styles for skills section specifically text color and hover effect

--- app/components/sections/skills.js
+++ app/components/sections/skills.js
@@ -28,16 +28,16 @@ export default function Skills(){
               <div className="ml-4 mt-2">
               {Object.entries(skills).map(([key, value]) => (
                     <div key={key} className="flex items-center space-y-4 ">
-                        <div className="mr-5 mt-3 text-gray-400 font-semibold ">
+                        <div className="mr-5 mt-3 text-gray-300 font-semibold ">
                             {key}:
                         </div>
                         <div className="flex space-x-3">
                             {value.map((item) => (
-                                <div key={item} className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-200/10 hover:ring-gray-100/20 text-center align-middle flex items-center justify-center">
-                                <a href="#" className="font-semibold text-gray-500 hidden m:block lg:block text-center">
+                                <div key={item} className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-200 ring-1 ring-blue-900 hover:ring-blue-400">
+                                <div className="font-semibold text-blue-200 hover:text-blue-300 hidden m:block lg:block text-center">
                                   <span aria-hidden="true" className="absolute inset-0" />
                                   {item}
-                                </a>
+                                </div>
                               </div>
                             ))}
                         </div>
