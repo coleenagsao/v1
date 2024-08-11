474267a6113f19da346c1bd83df5d4caa0178b6a
Coleen Agsao <caagsao@up.edu.ph>
2024-08-12 00:06:28 +0800

    chore: delete unused svgs

--- public/icons/index.js
+++
@@ -1,7 +0,0 @@
-export { default as IconLogo } from './logo';
-export { default as Github } from './github';
-export { default as LinkedIn } from './linkedin';
-export { default as Icon } from './icon';
-export { default as Polygon } from './polygon';
-export { default as Arrow } from './arrow';
-export { default as Redirect } from './redirect';
